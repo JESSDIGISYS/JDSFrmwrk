04391a6941b8abc763be9574a1135074de5087ec
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2024-06-11 19:11:32 -0400

    Added template folder and extensions

---
+++ src/Template/CssExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace JDS\Template;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class CssExtension extends AbstractExtension
+{
+	public function getFunctions(): array
+	{
+		return [
+			new TwigFunction("inline_css", [$this, "inlineCss"]),
+		];
+	}
+
+	public function inlineCss(string $filePath): string|bool
+	{
+		return $this->getCssContent($filePath);
+	}
+
+	private function getCssContent($filePath): string|bool
+	{
+		if (file_exists($filePath)) {
+			return file_get_contents($filePath);
+		}
+		return '';
+	}
+}
---
+++ src/Template/TwigFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace JDS\Template;
+
+use JDS\Session\SessionInterface;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
+
+
+class TwigFactory
+{
+	public function __construct(
+		private readonly SessionInterface $session,
+		private string                    $templatesPath
+	)
+	{
+	}
+
+	public function create(): Environment
+	{
+//		dd(dirname(__DIR__, 3) . '/templates');
+		// instantiate FileSysteLoader with templates path
+		$loader = new FilesystemLoader(dirname(__DIR__,2) . '/templates');
+
+		// instantiate Twig Environment with loader
+		$twig = new Environment($loader, [
+			'debug' => true,
+			'cache' => false,
+		]);
+
+		$baseDir = '/';
+
+		// add new twig session() function to Environment
+		$twig->addExtension(new DebugExtension());
+		$twig->addExtension(new AssetExtension($baseDir));
+		$twig->addFunction(new TwigFunction('session', [$this, 'getSession']));
+
+		return $twig;
+	}
+
+	public function getSession(): SessionInterface
+	{
+		return $this->session;
+	}
+
+//	public function assetFunction($path): string {
+//		return dirname(__DIR__, 3) . '/public/' . $path;
+//	}
+}
