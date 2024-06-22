1b621a4dac6394dd671a2931596e5af46704819f
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2024-06-21 20:32:15 -0400

    Authenticate, ExtractRouteInfo

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -2,7 +2,6 @@
 
 namespace JDS\Http\Middleware;
 
-use Firebase\JWT\JWT;
 use JDS\Http\RedirectResponse;
 use JDS\Http\Request;
 use JDS\Http\Response;
@@ -10,8 +9,6 @@ use JDS\Session\SessionInterface;
 
 class Authenticate implements MiddlewareInterface
 {
-	private string $jwtKey; // todo move all JWT to login controller on client
-
 	public function __construct(
 		private SessionInterface $session
 	)
@@ -27,31 +24,8 @@ class Authenticate implements MiddlewareInterface
 
 			return new RedirectResponse($requestHandler->getContainer()->get('routePath') . '/login');
 		}
-		$this->setJwtKey($request->getServerVariable('JWTSECRET'));
-
-		$this->session->set('jwttoken', $this->generateJWT($this->session->get('auth_id'), $request)) ;
 
 		return $requestHandler->handle($request);
-	}
-
-	private function generateJWT(string $user_id, Request $request) {
-		$payload = [
-			'iss' => $request->getServerVariable('HTTP_HOST'),
-			'aud' => $request->getServerVariable('SERVER_NAME'),
-			'iat' => time(), // Issued At Time
-			'nbf' => time(), // Not valid Before
-			'exp' => time() + (60 * 60 * 24 * 5), // expire after iat (5 days)
-			'data' => [
-				'user_id' => $user_id,
-			]
-		];
-
-		return JWT::encode($payload, $this->jwtKey, 'HS256');
-	}
-
-	public function setJwtKey(string $jwtKey): void
-	{
-		$this->jwtKey = $jwtKey;
 	}
 }
 
--- src/Http/Middleware/ExtractRouteInfo.php
+++ src/Http/Middleware/ExtractRouteInfo.php
@@ -45,7 +45,7 @@ class ExtractRouteInfo implements MiddlewareInterface
 				$request->setRouteHandler($routeInfo[1]);
 
 				// set $request->routeHandlerArgs
-				$request->setRouteHandlerArgs($routeInfo[1][2]);
+				$request->setRouteHandlerArgs($routeInfo[1][2]) ?? $request->setRouteHandlerArgs($routeInfo[1]);
 
 				// inject route middleware on handler
 				if (is_array($routeInfo[1]) && isset($routeInfo[1][2])) {
