720b133c7752b772d815f0579fdfdbe0be3c5ad2
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2024-11-24 14:04:35 -0500

    Work on a menu service interface

---
+++ src/Dbal/MenuServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JDS\Dbal;
+
+interface MenuServiceInterface extends ServiceInterface
+{
+    public function getPrimaryMenuById(string $id): ?Entity;
+
+    public function getSecondaryMenusByPrimaryMenuId(string $id): array;
+
+    public function getTertiaryMenusBySecondaryMenuId(string $id): array;
+
+}
---
+++ src/Dbal/ServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace JDS\Dbal;
+
+interface ServiceInterface
+{
+    public function lookup(string $id): ?Entity;
+    public function save(Entity $entity): bool;
+    public function delete(Entity $entity): bool;
+}
