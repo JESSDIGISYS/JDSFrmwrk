2b44302a750f0e6db6186c4433e138002b9d0686
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2024-12-13 14:28:11 -0500

    Update Authenticate to make sure a user logged in is actually in the user table

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -17,6 +17,20 @@ class Authenticate implements MiddlewareInterface
 
 	public function process(Request $request, RequestHandlerInterface $requestHandler): Response
 	{
+        // first check to see if the user logged in is actually in the user table
+        $sql = "SELECT
+                user_id 
+            FROM 
+                users 
+            WHERE 
+                user_id = :user_id; ";
+        $stmt = $requestHandler->getContainer()->get('connection')->prepare($sql);
+        $stmt->bindValue(':user_id', $this->session->get('user_id'));
+        $stmt->executeQuery();
+        $user = $stmt->fetchAssociative();
+        if (!$user) {
+            $this->session->remove('auth_id');
+        }
 		$this->session->start();
 
 		if (!$this->session->isAuthenticated()) {
