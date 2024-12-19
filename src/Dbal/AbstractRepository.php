116485a68731917137e4dadb966c63f0439a5e3c
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2024-12-19 17:30:59 -0500

    Add abstract repository to give more functionality to the bindValue method

---
+++ src/Dbal/AbstractRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JDS\Dbal;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\ParameterType;
+use Doctrine\DBAL\Statement;
+
+abstract class AbstractRepository
+{
+    /**
+     * @throws Exception
+     */
+    public function bind(Statement $statement, string $parameter, $value, $type = null): void {
+        switch (is_null($type)) {
+            case is_int($value):
+                $type = ParameterType::INTEGER;
+                break;
+
+            case is_bool($value):
+                $type = ParameterType::BOOLEAN;
+                break;
+
+            case is_null($value):
+                $type = ParameterType::NULL;
+                break;
+
+            default:
+                $type = ParameterType::STRING;
+        }
+        $statement->bindValue($parameter, $value, $type);
+    }
+}
