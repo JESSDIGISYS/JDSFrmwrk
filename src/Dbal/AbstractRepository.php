9be560acaf4ec00874ba40fb8b2bafb6376512ab
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2024-12-20 21:14:36 -0500

    Add check table exists method to abstract repository

--- src/Dbal/AbstractRepository.php
+++ src/Dbal/AbstractRepository.php
@@ -2,6 +2,7 @@
 
 namespace JDS\Dbal;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception;
 use Doctrine\DBAL\ParameterType;
 use Doctrine\DBAL\Statement;
@@ -30,4 +31,29 @@ abstract class AbstractRepository
         }
         $statement->bindValue($parameter, $value, $type);
     }
+
+    public function checkTableExists(Connection $connection, string $database, string $table): bool
+    {
+        try {
+            // Assuming $pdo is your PDO connection
+            $sql = "SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = :databaseName AND TABLE_NAME = :tableName;";
+
+            $stmt = $connection->prepare($sql);
+            $stmt->bindValue('databaseName', $database);
+            $stmt->bindValue('tableName', $table);
+            $rows = $stmt->executeQuery();
+
+            if ($rows->fetchFirstColumn()) {
+                // The table exists, you can continue your operations here.
+                return true;
+            } else {
+                // The table does not exist.
+                // You can notify the user about this and stop execution, or handle this situation in any other way that suits your work.
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new \Exception($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
 }
