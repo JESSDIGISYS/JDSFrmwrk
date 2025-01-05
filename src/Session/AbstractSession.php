b74d990aaedc55400f0f134bdb08e0b95a951d3b
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2025-01-05 14:51:12 -0500

    Fix AbstractSession

--- src/Session/AbstractSession.php
+++ src/Session/AbstractSession.php
@@ -10,7 +10,7 @@ class AbstractSession
     protected function configuration(): void
     {
         // Set session save path.
-        $savePath = getenv('SESSION_SAVE_PATH') ?: ini_get('session.save_path');
+        $savePath = $_ENV['SESSION_SAVE_PATH'] ?: ini_get('session.save_path');
 
         // Ensure the session save path exists.
         if (!is_dir($savePath) && !mkdir($savePath, 0777, true) && !is_dir($savePath)) {
@@ -27,16 +27,16 @@ class AbstractSession
         session_save_path($savePath);
 
         // session name
-        session_name(getenv('SESSION_NAME') ?: ini_get('session.name'));
+        session_name($_ENV['SESSION_NAME'] ?: ini_get('session.name'));
 
         // Set session cookie parameters.
         session_set_cookie_params([
-            'lifetime' => getenv('SESSION_COOKIE_LIFETIME') ?: 300,          // Default to 5 minutes.
-            'path' => getenv('SESSION_COOKIE_PATH') ?: '/',               // Default to root path.
-            'domain' => getenv('SESSION_COOKIE_DOMAIN') ?: '',            // Default to current domain.
-            'secure' => getenv('SESSION_COOKIE_SECURE') === 'true',       // True for secure (HTTPS).
-            'httponly' => getenv('SESSION_COOKIE_HTTPONLY') === 'true',   // True for HTTP-only cookies.
-            'samesite' => getenv('SESSION_COOKIE_SAMESITE') ?: 'Lax',     // Default to "Lax".
+            'lifetime' => $_ENV['SESSION_COOKIE_LIFETIME'] ?: 300,          // Default to 5 minutes.
+            'path' => $_ENV['SESSION_COOKIE_PATH'] ?: '/',               // Default to root path.
+            'domain' => $_ENV['SESSION_COOKIE_DOMAIN'] ?: '',            // Default to current domain.
+            'secure' => $_ENV['SESSION_COOKIE_SECURE'] === 'true',       // True for secure (HTTPS).
+            'httponly' => $_ENV['SESSION_COOKIE_HTTPONLY'] === 'true',   // True for HTTP-only cookies.
+            'samesite' => $_ENV['SESSION_COOKIE_SAMESITE'] ?: 'Lax',     // Default to "Lax".
         ]);
 
     }
